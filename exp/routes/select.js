60d9a05a48b03ef25ff4422c3ce2ec43a74802a2
JacobMJones <mrjacobtaiwan@gmail.com>
2019-03-12 19:51:40 -0400

    added to select

--- exp/routes/select.js
+++ exp/routes/select.js
@@ -1,19 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-
-router.get('/', function(req, res, next) {
-var connection = req.app.get('connection')
-console.log(req.query)
-var table = req.query.table
-var column = req.query.column
- connection.query(`SELECT ${column} FROM ${table}`, function(error, results, fields) {
-    if (error) throw error;
-    res.send({results});
-  });
-
- 
+router.get("/", function(req, res, next) {
+  var connection = req.app.get("connection");
   
+  var table = req.query.table;
+  var column = req.query.column;
+  var where = req.query.where;
+  var whereDefinition = req.query.whereDefinition;
+
+  console.log("table", table, "column", column, "where", where, "whereDefinition", whereDefinition);
+  connection.query(
+    `SELECT ${column} FROM ${table} WHERE ${whereDefinition} = ${where}`,
+    function(error, results, fields) {
+      if (error) throw error;
+      res.send({ results });
+    }
+  );
 });
 
 module.exports = router;
--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,14 +4,14 @@ import axios from "axios";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {table:'cats'};
   }
   componentDidMount() {}
 
-  select = (table, column) => {
+  select = (table, column, where, whereDefinition) => {
     axios
       .get("http://localhost:3000/select", {
-        params: { table: table, column: column }
+        params: { table: table, column: column, where:where, whereDefinition:whereDefinition }
       })
       .then(function(response) {
         console.log(response);
@@ -26,6 +26,7 @@ class App extends Component {
       <div>
         Table
         <input
+          value="cats"
           onChange={e => {
             this.setState({ table: e.target.value }, () => {
               console.log(this.state);
@@ -42,9 +43,24 @@ class App extends Component {
           }}
         />
         <br />
+        Where
+        <input
+          onChange={e => {
+            this.setState({ where: e.target.value }, () => {
+              console.log(this.state);
+            });
+          }}
+        /> =  <input
+        onChange={e => {
+          this.setState({ whereDefinition: e.target.value }, () => {
+            console.log(this.state);
+          });
+        }}
+      />
+        <br />
         <button
           onClick={() => {
-            this.select(this.state.table, this.state.column);
+            this.select(this.state.table, this.state.column, this.state.where, this.state.whereDefinition);
           }}
         >
           SELECT
